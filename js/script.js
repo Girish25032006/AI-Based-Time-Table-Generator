ead28d396f0247d28260f802a220d1d361fc7701
Girish25032006 <girishg.tecacc2006@gmail.com>
2026-07-25 22:27:49 +0530

    added depart and dash

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    // Temporary Login
+    window.location.href = "dashboard.html";
+
+});
